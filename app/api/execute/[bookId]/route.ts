85d313e4944d5bb3cca0facbcad95162052db258
Kush Patel <68998452+libkush@users.noreply.github.com>
2025-03-01 02:56:15 +0530

    fix: updated preview endpoint for chapter

--- app/api/execute/[bookId]/route.ts
+++ app/api/execute/[bookId]/route.ts
@@ -2,31 +2,31 @@ import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest, { params }: { params: { bookId: string } }) {
-  const { bookId } = params;
+        const { bookId } = params;
 
-  try {
-    const formData = await req.formData();
+        try {
+                const formData = await req.formData();
 
-    const data = new FormData();
-    formData.forEach((value, key) => {
-      data.append(key, value);
-    });
+                const data = new FormData();
+                formData.forEach((value, key) => {
+                        data.append(key, value);
+                });
 
-    const apiUrl = `${process.env.BASE_URL}/render/${bookId}`;
+                const apiUrl = `${process.env.BASE_URL}/preview/${bookId}`;
 
-    const response = await axios.post(apiUrl, data, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    });
+                const response = await axios.post(apiUrl, data, {
+                        headers: {
+                                "Content-Type": "multipart/form-data",
+                        },
+                });
 
-    const result = response.data;
-    return NextResponse.json(result, { status: response.status });
-  } catch (error: any) {
-    console.error("Error proxying request:", error.message);
-    return NextResponse.json(
-      { error: error.message || "Internal server error" },
-      { status: 500 }
-    );
-  }
+                const result = response.data;
+                return NextResponse.json(result, { status: response.status });
+        } catch (error: any) {
+                console.error("Error proxying request:", error.message);
+                return NextResponse.json(
+                        { error: error.message || "Internal server error" },
+                        { status: 500 }
+                );
+        }
 }
